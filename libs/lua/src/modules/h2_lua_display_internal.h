303586b6697d5c59654ef126fa8b4f234214d6d8
Sid9017 <13128630+Sid9017@users.noreply.github.com>
2026-09-15 12:33:43 +0800

    lua: Isolate the existing Display implementation

---
+++ libs/lua/src/modules/h2_lua_display_internal.h
@@ -0,0 +1,8 @@
+#ifndef H2_LUA_DISPLAY_INTERNAL_H
+#define H2_LUA_DISPLAY_INTERNAL_H
+
+#include "../runtime/h2_lua_internal.h"
+
+int h2_lua_push_display_proxy(lua_State *state, h2_lua_job_t *job);
+
+#endif
